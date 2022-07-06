d06d8ca3051e8a962010e99d13878360f3ab7b8a
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-06 21:12:56 +0300

    Add parametrized multiplication tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -14,3 +14,19 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
     assert 'division by zero' in str(e.value)
+
+#Adding parameterized multiplication tests
+#Creating tuples for future tests
+products = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0),
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+#Creating a parameterized testcase
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
