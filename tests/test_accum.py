8f6659f6da3fc7131e0275f6995f3418b56cfaee
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-16 20:00:03 +0300

    Add custom accum marker to some accumulator tests

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -1,25 +1,36 @@
 import pytest
 from stuff.accum import Accumulator
 
-#Using fixtures
+# Using fixtures
+
+
 @pytest.fixture
 def accum():
     return Accumulator()
+
+
+@pytest.mark.accum
 def test_accumulator_init(accum):
     assert accum.count == 0
 
+
+@pytest.mark.accum
 def test_accumulator_add_one(accum):
     accum.add()
     assert accum.count == 1
 
+
+@pytest.mark.accum
 def test_accumulator_add_three(accum):
     accum.add(3)
     assert accum.count == 3
+
 
 def test_accumulator_add_twice(accum):
     accum.add()
     accum.add()
     assert accum.count == 2
+
 
 def test_accumulator_cannot_set_count_directly(accum):
     with pytest.raises(AttributeError, match=r"can't set attribute") as e:
